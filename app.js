e31191cb23f5a72f001762ed569a69a7038ba92a
Elvan Hidayat <elvanhd@gmail.com>
2023-03-31 03:51:40 +0700

    create a pug template

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.get('/', (req, res, next) => {
-    res.status(200).send('Hello, World!');
+    res.status(200).render('home');
 });
 
 app.listen(port, () => console.log('Server listening on port ' + port));
