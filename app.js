34316bf4f4192c757bcdb711373e5ee62b5aae5b
Elvan Hidayat <elvanhd@gmail.com>
2023-03-31 03:20:40 +0700

    render some content

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 
 const port = 3000;
 
+app.get('/', (req, res, next) => {
+    res.status(200).send('Hello, World!');
+});
+
 app.listen(port, () => console.log('Server listening on port ' + port));
