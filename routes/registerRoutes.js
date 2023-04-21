5a052c1eb0b7e82806325c59df23475a36405a67
Elvan Hidayat <elvanhd@gmail.com>
2023-04-21 17:29:04 +0700

    checking for empty fields

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -7,9 +7,19 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    console.log(req.body);
+    var firstName = req.body.firstName.trim();
+    var lastName = req.body.lastName.trim();
+    var username = req.body.username.trim();
+    var email = req.body.email.trim();
+    var password = req.body.password;
 
-    res.status(200).render('register');
+    var payload = req.body;
+
+    if (firstName && lastName && username && email && password) {
+    } else {
+        payload.errorMessage = 'Make sure each field has a valid value.';
+        res.status(200).render('register', payload);
+    }
 });
 
 module.exports = router;
