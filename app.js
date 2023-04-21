339fd809ab391e1fea7d9fcceaf13991c20f1f7b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-21 19:38:18 +0700

    connecting to the database

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const middleware = require('./middleware');
 
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
+
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/social-networking-app';
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
 
 const app = express();
 
@@ -28,4 +38,13 @@ app.get('/', middleware.requireLogin, (req, res, next) => {
     res.status(200).render('home', payload);
 });
 
-app.listen(port, () => console.log('Server listening on port ' + port));
+mongoose
+    .connect(MONGODB_URL)
+    .then(() => {
+        console.log('Database connection successful');
+
+        app.listen(port, () => console.log('Server listening on port ' + port));
+    })
+    .catch((err) => {
+        console.log('database connection error ' + err);
+    });
