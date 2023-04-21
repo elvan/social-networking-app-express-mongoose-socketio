be39e8c7fea60ea9d36a8b8cbcdd5237aabec32f
Elvan Hidayat <elvanhd@gmail.com>
2023-04-22 03:57:01 +0700

    creating a mixin

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema(
         username: { type: String, required: true, trim: true, unique: true },
         email: { type: String, required: true, trim: true, unique: true },
         password: { type: String, required: true },
-        profilePic: { type: String, default: '/images/profilePic.png' },
+        profilePic: { type: String, default: '/images/profilePic.jpeg' },
     },
     {
         timestamps: true,
