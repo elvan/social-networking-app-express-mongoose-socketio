2a061e3eeaf6d1371a7f462ec7d23e5aaee2d063
Elvan Hidayat <elvanhd@gmail.com>
2023-04-22 04:33:04 +0700

    enabling the submit button

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,15 @@
 $('#postTextarea').keyup((event) => {
     var textbox = $(event.target);
     var value = textbox.val().trim();
-    console.log(value);
+
+    var submitButton = $('#submitPostButton');
+
+    if (submitButton.length == 0) return alert('No submit button found');
+
+    if (value == '') {
+        submitButton.prop('disabled', true);
+        return;
+    }
+
+    submitButton.prop('disabled', false);
 });
