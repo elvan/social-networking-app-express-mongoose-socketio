74bdf59ae1bace2aba476eee3ee940d1858c8cd5
Elvan Hidayat <elvanhd@gmail.com>
2023-04-22 20:38:14 +0700

    sending bad request error when the data is invalid

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,11 @@ const router = express.Router();
 router.get('/', (req, res, next) => {});
 
 router.post('/', async (req, res, next) => {
+    if (!req.body.content) {
+        console.log('Content param not sent with request');
+        return res.sendStatus(400);
+    }
+
     res.status(200).send('it worked');
 });
 
