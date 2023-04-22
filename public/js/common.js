e37bae9bd862416b8fc6ece002d4b488de0c0a2f
Elvan Hidayat <elvanhd@gmail.com>
2023-04-23 06:57:58 +0700

    updating the button to show the number of likes

--- public/js/common.js
+++ public/js/common.js
@@ -40,7 +40,7 @@ $(document).on('click', '.likeButton', (event) => {
         url: `/api/posts/${postId}/like`,
         type: 'PUT',
         success: (postData) => {
-            console.log(postData.likes.length);
+            button.find('span').text(postData.likes.length || '');
         },
     });
 });
@@ -73,7 +73,9 @@ function createPostHtml(postData) {
                 </div>
                 <div class='postContentContainer'>
                     <div class='header'>
-                        <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+                        <a href='/profile/${
+                            postedBy.username
+                        }' class='displayName'>${displayName}</a>
                         <span class='username'>@${postedBy.username}</span>
                         <span class='date'>${timestamp}</span>
                     </div>
@@ -94,6 +96,7 @@ function createPostHtml(postData) {
                         <div class='postButtonContainer'>
                             <button class='likeButton'>
                                 <i class='far fa-heart'></i>
+                                <span>${postData.likes.length || ''}</span>
                             </button>
                         </div>
                     </div>
