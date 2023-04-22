a4fda98c8b4cbbc77a8a3fdab8461baafc1c4e1e
Elvan Hidayat <elvanhd@gmail.com>
2023-04-22 22:38:51 +0700

    styling the action buttons

--- public/js/common.js
+++ public/js/common.js
@@ -43,7 +43,7 @@ function createPostHtml(postData) {
                 </div>
                 <div class='postContentContainer'>
                     <div class='header'>
-                        <a href='/profile/${postedBy.username}'>${displayName}</a>
+                        <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
                         <span class='username'>@${postedBy.username}</span>
                         <span class='date'>${timestamp}</span>
                     </div>
@@ -51,6 +51,21 @@ function createPostHtml(postData) {
                         <span>${postData.content}</span>
                     </div>
                     <div class='postFooter'>
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='far fa-comment'></i>
+                            </button>
+                        </div>
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='fas fa-retweet'></i>
+                            </button>
+                        </div>
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='far fa-heart'></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
