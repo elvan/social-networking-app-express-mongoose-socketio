a96d94d3635dc6e1aa4a9cad71e4caf9ddd064cd
Elvan Hidayat <elvanhd@gmail.com>
2023-04-23 06:04:33 +0700

    like button click handler

--- public/js/common.js
+++ public/js/common.js
@@ -30,6 +30,10 @@ $('#submitPostButton').click(() => {
     });
 });
 
+$(document).on('click', '.likeButton', () => {
+    alert('button clicked');
+});
+
 function createPostHtml(postData) {
     var postedBy = postData.postedBy;
 
@@ -67,7 +71,7 @@ function createPostHtml(postData) {
                             </button>
                         </div>
                         <div class='postButtonContainer'>
-                            <button>
+                            <button class='likeButton'>
                                 <i class='far fa-heart'></i>
                             </button>
                         </div>
