1b72741bbc48cd161563562f4b2d3e027ca14d1b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-22 21:35:13 +0700

    outputting the content and the user info

--- public/js/common.js
+++ public/js/common.js
@@ -31,5 +31,29 @@ $('#submitPostButton').click(() => {
 });
 
 function createPostHtml(postData) {
-    return postData.content;
+    var postedBy = postData.postedBy;
+    var displayName = postedBy.firstName + ' ' + postedBy.lastName;
+    var timestamp = postData.createdAt;
+
+    return `
+        <div class='post'>
+            <div class='mainContentContainer'>
+                <div class='userImageContainer'>
+                    <img src='${postedBy.profilePic}'>
+                </div>
+                <div class='postContentContainer'>
+                    <div class='header'>
+                        <a href='/profile/${postedBy.username}'>${displayName}</a>
+                        <span class='username'>@${postedBy.username}</span>
+                        <span class='date'>${timestamp}</span>
+                    </div>
+                    <div class='postBody'>
+                        <span>${postData.content}</span>
+                    </div>
+                    <div class='postFooter'>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
 }
