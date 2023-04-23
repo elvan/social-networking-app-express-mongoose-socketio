44daa07ea08d333f480b8a8996d15f34f5eab87b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-23 21:05:13 +0700

    getting the id of the post to delete

--- public/js/common.js
+++ public/js/common.js
@@ -56,6 +56,18 @@ $('#replyModal').on('show.bs.modal', (event) => {
 
 $('#replyModal').on('hidden.bs.modal', () => $('#originalPostContainer').html(''));
 
+$('#deletePostModal').on('show.bs.modal', (event) => {
+    var button = $(event.relatedTarget);
+    var postId = getPostIdFromElement(button);
+    $('#deletePostButton').data('id', postId);
+
+    console.log($('#deletePostButton').data().id);
+
+    // $.get("/api/posts/" + postId, results => {
+    //     outputPosts(results.postData, $("#originalPostContainer"));
+    // })
+});
+
 $(document).on('click', '.likeButton', (event) => {
     var button = $(event.target);
     var postId = getPostIdFromElement(button);
