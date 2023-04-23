97c00997b06a8c4031fa0daf26046af8f5be83ed
Elvan Hidayat <elvanhd@gmail.com>
2023-04-23 07:04:17 +0700

    passing the user logged in info to the client

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.get('/', middleware.requireLogin, (req, res, next) => {
     var payload = {
         pageTitle: 'Home',
         userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user),
     };
 
     res.status(200).render('home', payload);
