d1ff98edc372d008c599d9b1864da2c2b62e92b6
Elvan Hidayat <elvanhd@gmail.com>
2023-04-23 23:50:52 +0700

    accessing the profile page

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const User = require('../schemas/UserSchema');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -12,5 +14,35 @@ router.get('/', (req, res, next) => {
 
     res.status(200).render('profilePage', payload);
 });
+
+router.get('/:username', async (req, res, next) => {
+    var payload = await getPayload(req.params.username, req.session.user);
+    console.log(payload);
+
+    res.status(200).render('profilePage', payload);
+});
+
+async function getPayload(username, userLoggedIn) {
+    var user = await User.findOne({ username: username });
+
+    if (user == null) {
+        user = await User.findById(username);
+
+        if (user == null) {
+            return {
+                pageTitle: 'User not found',
+                userLoggedIn: userLoggedIn,
+                userLoggedInJs: JSON.stringify(userLoggedIn),
+            };
+        }
+    }
+
+    return {
+        pageTitle: user.username,
+        userLoggedIn: userLoggedIn,
+        userLoggedInJs: JSON.stringify(userLoggedIn),
+        profileUser: user,
+    };
+}
 
 module.exports = router;
