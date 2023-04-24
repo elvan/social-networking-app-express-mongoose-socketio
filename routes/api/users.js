b3f2d26d83a8864efd29f5d9f2160c8f166ee5f6
Elvan Hidayat <elvanhd@gmail.com>
2023-04-24 09:42:47 +0700

    following and unfollowing the user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,36 @@
 const express = require('express');
 
+const User = require('../../schemas/UserSchema');
+
 const router = express.Router();
 
 router.put('/:userId/follow', async (req, res, next) => {
-    res.status(200).send('Shrek');
+    var userId = req.params.userId;
+
+    var user = await User.findById(userId);
+
+    if (user == null) return res.sendStatus(404);
+
+    var isFollowing = user.followers && user.followers.includes(req.session.user._id);
+    var option = isFollowing ? '$pull' : '$addToSet';
+
+    req.session.user = await User.findByIdAndUpdate(
+        req.session.user._id,
+        { [option]: { following: userId } },
+        { new: true }
+    ).catch((error) => {
+        console.log(error);
+        res.sendStatus(400);
+    });
+
+    User.findByIdAndUpdate(userId, { [option]: { followers: req.session.user._id } }).catch(
+        (error) => {
+            console.log(error);
+            res.sendStatus(400);
+        }
+    );
+
+    res.status(200).send(req.session.user);
 });
 
 module.exports = router;
