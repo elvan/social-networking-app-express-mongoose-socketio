b11a92859ad7e43eecdc2f352616e3f32ecf4b10
Elvan Hidayat <elvanhd@gmail.com>
2023-04-25 06:28:15 +0700

    pinning posts

--- public/js/common.js
+++ public/js/common.js
@@ -65,6 +65,12 @@ $('#deletePostModal').on('show.bs.modal', (event) => {
     $('#deletePostButton').data('id', postId);
 });
 
+$('#confirmPinModal').on('show.bs.modal', (event) => {
+    var button = $(event.relatedTarget);
+    var postId = getPostIdFromElement(button);
+    $('#pinPostButton').data('id', postId);
+});
+
 $('#deletePostButton').click((event) => {
     var postId = $(event.target).data('id');
 
@@ -73,6 +79,24 @@ $('#deletePostButton').click((event) => {
         type: 'DELETE',
         success: (data, status, xhr) => {
             if (xhr.status != 202) {
+                alert('could not delete post');
+                return;
+            }
+
+            location.reload();
+        },
+    });
+});
+
+$('#pinPostButton').click((event) => {
+    var postId = $(event.target).data('id');
+
+    $.ajax({
+        url: `/api/posts/${postId}`,
+        type: 'PUT',
+        data: { pinned: true },
+        success: (data, status, xhr) => {
+            if (xhr.status != 204) {
                 alert('could not delete post');
                 return;
             }
@@ -313,7 +337,10 @@ function createPostHtml(postData, largeFont = false) {
 
     var buttons = '';
     if (postData.postedBy._id == userLoggedIn._id) {
-        buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='fas fa-times'></i></button>`;
+        buttons = `
+            <button data-id="${postData._id}" data-toggle="modal" data-target="#confirmPinModal"><i class='fas fa-thumbtack'></i></button>
+            <button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='fas fa-times'></i></button>
+        `;
     }
 
     return `
