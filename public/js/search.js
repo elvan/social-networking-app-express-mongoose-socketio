f6ae1787c6a5aa141c3901ce51e5b8922d5ab101
Elvan Hidayat <elvanhd@gmail.com>
2023-04-25 10:32:17 +0700

    searching for posts

--- public/js/search.js
+++ public/js/search.js
@@ -12,7 +12,15 @@ $('#searchBox').keydown((event) => {
         if (value == '') {
             $('.resultsContainer').html('');
         } else {
-            console.log(value);
+            search(value, searchType);
         }
     }, 1000);
 });
+
+function search(searchTerm, searchType) {
+    var url = searchType == 'users' ? '/api/users' : '/api/posts';
+
+    $.get(url, { search: searchTerm }, (results) => {
+        console.log(results);
+    });
+}
