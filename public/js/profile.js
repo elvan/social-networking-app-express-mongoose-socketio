5ee016a3bec41cdac2b783d4cc1655e1a162199b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-25 09:46:29 +0700

    displaying the pinned post on the profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -7,6 +7,10 @@ $(document).ready(() => {
 });
 
 function loadPosts() {
+    $.get('/api/posts', { postedBy: profileUserId, pinned: true }, (results) => {
+        outputPinnedPost(results, $('.pinnedPostContainer'));
+    });
+
     $.get('/api/posts', { postedBy: profileUserId, isReply: false }, (results) => {
         outputPosts(results, $('.postsContainer'));
     });
@@ -15,5 +19,19 @@ function loadPosts() {
 function loadReplies() {
     $.get('/api/posts', { postedBy: profileUserId, isReply: true }, (results) => {
         outputPosts(results, $('.postsContainer'));
+    });
+}
+
+function outputPinnedPost(results, container) {
+    if (results.length == 0) {
+        container.hide();
+        return;
+    }
+
+    container.html('');
+
+    results.forEach((result) => {
+        var html = createPostHtml(result);
+        container.append(html);
     });
 }
