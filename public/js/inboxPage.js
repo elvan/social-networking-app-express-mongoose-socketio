08ff3c6e5e9551acadd617cda6a30a981a0e4241
Elvan Hidayat <elvanhd@gmail.com>
2023-04-26 09:15:47 +0700

    outputting and styling the chats

--- public/js/inboxPage.js
+++ public/js/inboxPage.js
@@ -9,5 +9,27 @@ $(document).ready(() => {
 });
 
 function outputChatList(chatList, container) {
-    console.log(chatList);
+    chatList.forEach((chat) => {
+        var html = createChatHtml(chat);
+        container.append(html);
+    });
+
+    if (chatList.length == 0) {
+        container.append("<span class='noResults'>Nothing to show.</span>");
+    }
+}
+
+function createChatHtml(chatData) {
+    var chatName = 'Chat name'; // TODO
+    var image = ''; // TODO
+    var latestMessage = 'This is the latest message';
+
+    return `
+        <a href='/messages/${chatData._id}' class='resultListItem'>
+            <div class='resultsDetailsContainer'>
+                <span class='heading'>${chatName}</span>
+                <span class='subText'>${latestMessage}</span>
+            </div>
+        </a>
+    `;
 }
