6a46a483f2fd94654abb21c42b6035ff82e0fda1
Elvan Hidayat <elvanhd@gmail.com>
2023-04-26 09:41:10 +0700

    showing and styling the chat images

--- public/js/inboxPage.js
+++ public/js/inboxPage.js
@@ -21,14 +21,15 @@ function outputChatList(chatList, container) {
 
 function createChatHtml(chatData) {
     var chatName = getChatName(chatData);
-    var image = ''; // TODO
+    var image = getChatImageElements(chatData);
     var latestMessage = 'This is the latest message';
 
     return `
         <a href='/messages/${chatData._id}' class='resultListItem'>
-            <div class='resultsDetailsContainer'>
-                <span class='heading'>${chatName}</span>
-                <span class='subText'>${latestMessage}</span>
+            ${image}
+            <div class='resultsDetailsContainer ellipsis'>
+                <span class='heading ellipsis'>${chatName}</span>
+                <span class='subText ellipsis'>${latestMessage}</span>
             </div>
         </a>
     `;
@@ -50,4 +51,26 @@ function getOtherChatUsers(users) {
     if (users.length == 1) return users;
 
     return users.filter((user) => user._id != userLoggedIn._id);
+}
+
+function getChatImageElements(chatData) {
+    var otherChatUsers = getOtherChatUsers(chatData.users);
+
+    var groupChatClass = '';
+    var chatImage = getUserChatImageElement(otherChatUsers[0]);
+
+    if (otherChatUsers.length > 1) {
+        groupChatClass = 'groupChatImage';
+        chatImage += getUserChatImageElement(otherChatUsers[1]);
+    }
+
+    return `<div class='resultsImageContainer ${groupChatClass}'>${chatImage}</div>`;
+}
+
+function getUserChatImageElement(user) {
+    if (!user || !user.profilePic) {
+        return alert('User passed into function is invalid');
+    }
+
+    return `<img src='${user.profilePic}' alt='User's profile pic'>`;
 }
