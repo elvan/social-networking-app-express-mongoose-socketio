eef209a9d8ba1b3d926e87b840ad62538a394e5b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-26 11:02:26 +0700

    adding the chat name modal popup and the ajax request

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,1 +1,16 @@
-$('#chatNameButton').click(() => {});
+$('#chatNameButton').click(() => {
+    var name = $('#chatNameTextbox').val().trim();
+
+    $.ajax({
+        url: '/api/chats/' + chatId,
+        type: 'PUT',
+        data: { chatName: name },
+        success: (data, status, xhr) => {
+            if (xhr.status != 204) {
+                alert('could not update');
+            } else {
+                location.reload();
+            }
+        },
+    });
+});
