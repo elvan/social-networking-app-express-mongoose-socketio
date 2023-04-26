82d58b04f47e1ba0d3447960e6d6e093e8e2d7b6
Elvan Hidayat <elvanhd@gmail.com>
2023-04-26 09:05:43 +0700

    getting the chats

--- public/js/inboxPage.js
+++ public/js/inboxPage.js
@@ -1,4 +1,12 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+    $.get('/api/chats', (data, status, xhr) => {
+        if (xhr.status == 400) {
+            alert('Could not get chat list.');
+        } else {
+            outputChatList(data, $('.resultsContainer'));
+        }
+    });
+});
 
 function outputChatList(chatList, container) {
     console.log(chatList);
