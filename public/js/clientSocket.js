acf99b0b8ec158eef7ea29005ea61716251885d4
Elvan Hidayat <elvanhd@gmail.com>
2023-04-27 12:54:08 +0700

    outputting and styling the notifications

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -7,8 +7,11 @@ socket.emit('setup', userLoggedIn);
 socket.on('connected', () => (connected = true));
 socket.on('message received', (newMessage) => messageReceived(newMessage));
 
-socket.on('notification received', (newNotification) => {
-    console.log('new notification');
+socket.on('notification received', () => {
+    $.get('/api/notifications/latest', (notificationData) => {
+        showNotificationPopup(notificationData);
+        refreshNotificationsBadge();
+    });
 });
 
 function emitNotification(userId) {
